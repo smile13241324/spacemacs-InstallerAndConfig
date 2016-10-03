68c8a57e99c06c32c53dd1b3d40f2f996dd1abde
smile13241324 <smile13241324@gmail.com>
2016-10-03 20:48:21 +0200

    Changed the emacs 25.1 build to include some of the newer features like webkit, cairo and module support.

--- spacemacsAutoInstall.sh
+++ spacemacsAutoInstall.sh
@@ -3,8 +3,8 @@
 # Set base path
 installBaseDir="${HOME}/.spacemacsInstall"
 
-# Install general depencies
-sudo apt-get install libncurses5-dev git silversearcher-ag silversearcher-ag-el curl wget vim build-essential cmake python-dev
+# Install general dependencies
+sudo apt-get install libncurses5-dev git silversearcher-ag silversearcher-ag-el curl wget vim build-essential cmake python-dev python3-dev libgtk-3-dev libwebkitgtk-3.0-dev libgtk-3-common libgtk-3-0
 
 # Git config
 git config --global user.name "smile13241324"
@@ -12,22 +12,23 @@ git config --global user.email "smile13241324@gmail.com"
 git config --global credential.helper cache
 
 # Build emacs package
-emacsBaseDir=${installBaseDir}/emacs
+emacsBaseDir="${installBaseDir}/emacs"
 sudo apt-get build-dep emacs24
 git clone git://git.savannah.gnu.org/emacs.git "${emacsBaseDir}"
-"${emacsBaseDir}/configure"
+"${emacsBaseDir}/configure --with-cairo --with-xwidgets --with-x-toolkit=gtk3 --with-modules"
 "${emacsBaseDir}/make"
 "${emacsBaseDir}/sudo make install"
 "${emacsBaseDir}/make clean"
 
 # Get adobe fonts 
-fontsBaseDir=${installBaseDir}/fonts
+fontsBaseDir="${installBaseDir}/fonts"
 git clone https://github.com/adobe-fonts/source-code-pro.git "${fontsBaseDir}"
 "${fontsBaseDir}/build.sh"
 cp "${fontsBaseDir}/target/OTF/* ~/.fonts"
 sudo fc-cache
 
 # Get newest spacemacs
+rm ~/.emacs.d -R
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 cp spacemacs ~/.spacemacs
 
@@ -45,7 +46,7 @@ Categories=Development;TextEditor;
 StartupWMClass=Emacs" >> ~/.local/share/applications/spacemacs.desktop
 
 # Install c++ layer dependencies
-sudo apt-get install clang clang-format clang-tidy libclang libclang-dev libc++ libc++-dev libc++-helpers libc++abi libc++abi-dev clang-modernize libclang-common-dev libclang-common libboost-all-dev libboost-all
+sudo apt-get install clang clang-format clang-tidy libclang libclang-dev libc++ libc++-dev libc++-helpers libc++abi libc++abi-dev clang-modernize libclang-common-dev libclang-common libboost-all-dev libboost-all lldb llvm 
 
 # Install python layer dependencies
 sudo apt-get install pytest
@@ -56,7 +57,7 @@ sudo apt-get install cscope
 pip install pycscope
 
 # Build gnu global
-globalBaseDir=${installBaseDir}/global
+globalBaseDir="${installBaseDir}/global"
 sudo apt-get install exuberant-ctags python-pygments
 wget -O "${globalBaseDir}/global.tar.gz" http://tamacom.com/global/global-6.5.5.tar.gz
 tar -zxvf global.tar.gz --directory "${globalBaseDir}"
