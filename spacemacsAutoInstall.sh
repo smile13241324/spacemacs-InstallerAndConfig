37e051efa2b1d78b41513dadb874f3125f5fa351
smile13241324 <smile13241324@gmail.com>
2016-10-04 16:33:04 +0200

    Fixed missing -y flag for apt-get

--- spacemacsAutoInstall.sh
+++ spacemacsAutoInstall.sh
@@ -4,7 +4,7 @@
 installBaseDir="${HOME}/.spacemacsInstall"
 
 # Install general dependencies
-sudo apt-get install libncurses5-dev git silversearcher-ag silversearcher-ag-el curl wget vim build-essential cmake python-dev python3-dev libgtk-3-dev libwebkitgtk-3.0-dev libgtk-3-common libgtk-3-0
+sudo apt-get install -y libncurses5-dev git silversearcher-ag silversearcher-ag-el curl wget vim build-essential cmake python-all python-all-dev libgtk-3-dev python- libwebkitgtk-3.0-dev libgtk-3-common libgtk-3-0 autoconf automake gdb
 
 # Git config
 git config --global user.name "smile13241324"
@@ -13,7 +13,7 @@ git config --global credential.helper cache
 
 # Build emacs package
 emacsBaseDir="${installBaseDir}/emacs"
-sudo apt-get build-dep emacs24
+sudo apt-get build-dep -y emacs24
 git clone git://git.savannah.gnu.org/emacs.git "${emacsBaseDir}"
 "${emacsBaseDir}/configure --with-cairo --with-xwidgets --with-x-toolkit=gtk3 --with-modules"
 "${emacsBaseDir}/make"
@@ -46,19 +46,20 @@ Categories=Development;TextEditor;
 StartupWMClass=Emacs" >> ~/.local/share/applications/spacemacs.desktop
 
 # Install c++ layer dependencies
-sudo apt-get install clang clang-format clang-tidy libclang libclang-dev libc++ libc++-dev libc++-helpers libc++abi libc++abi-dev clang-modernize libclang-common-dev libclang-common libboost-all-dev libboost-all lldb llvm 
+sudo apt-get install -y clang clang-format clang-tidy libclang-dev libclang1 libc++1 libc++-dev libc++-helpers libc++abi1 libc++abi-dev libc++-test libc++abi-test libboost-all-dev lldb llvm llvm-dev llvm-runtime
 
 # Install python layer dependencies
-sudo apt-get install pytest
+sudo apt-get install -y python-pytest python-pip python-mock python-setuptools-doc
+pip install --upgrade pip
 pip install jedi json-rpc service_factory autoflake hy
 
 # Install cscope dependencies
-sudo apt-get install cscope
+sudo apt-get install -y cscope cscope-el
 pip install pycscope
 
 # Build gnu global
 globalBaseDir="${installBaseDir}/global"
-sudo apt-get install exuberant-ctags python-pygments
+sudo apt-get install -y exuberant-ctags python-pygments
 wget -O "${globalBaseDir}/global.tar.gz" http://tamacom.com/global/global-6.5.5.tar.gz
 tar -zxvf global.tar.gz --directory "${globalBaseDir}"
 "${globalBaseDir}/configure --with-exuberant-ctags=/usr/bin/ctags"
