a0ef972aa95e136559e53e2185c14bb27ec627f2
smile13241324 <smile13241324@gmail.com>
2016-10-06 12:39:33 +0200

    Changed private setupGit.sh to also set push behaviour

--- private/setupGit.sh
+++ private/setupGit.sh
@@ -4,3 +4,4 @@
 git config --global user.name "smile13241324"
 git config --global user.email "smile13241324@gmail.com"
 git config --global credential.helper cache
+git config --global push.default simple
