d937e0dc918b14b26fa19b2e94d8575d0047c3d0
smile13241324 <smile13241324@gmail.com>
2017-04-08 13:28:38 +0200

    Adjust manjaro installer to be compatible with manjaro 17.0.1 and later

--- spacemacsAutoInstall_manjaro.sh
+++ spacemacsAutoInstall_manjaro.sh
@@ -2,8 +2,7 @@
 
 # Install external packages
 sudo pacman -Syyu
-sudo pacman -S git emacs the_silver_searcher vim cmake extra-cmake-modules python autoconf automake gdb gdb-common lldb shellcheck sbcl adobe-source-code-pro-fonts clang clang-tools-extra libc++ libc++abi boost boost-libs llvm llvm-libs python-pytest python-pip python-mock python-setuptools cscope npm nodejs nodejs-less npm-check-updates luarocks docker ctags python-pygments fish fisherman maven eclipse-java visualvm openjdk8-doc jdk8-openjdk gnuplot go go-tools
-sudo pacman -S "$(pacman -Ssq texlive*)"
+sudo pacman -S git tcl tk emacs the_silver_searcher vim wget curl cmake extra-cmake-modules python autoconf automake gdb gdb-common lldb shellcheck sbcl adobe-source-code-pro-fonts clang clang-tools-extra boost boost-libs llvm llvm-libs python-pytest python-pip python-mock python-setuptools cscope npm nodejs nodejs-less npm-check-updates luarocks docker ctags python-pygments fish fisherman maven eclipse-java visualvm openjdk8-doc jdk8-openjdk gnuplot go go-tools texlive-bin texlive-core texlive-fontsextra texlive-formatsextra texlive-games texlive-genericextra texlive-htmlxml texlive-humanities texlive-langchinese texlive-langcjk texlive-langcyrillic texlive-langextra texlive-langgreek texlive-langjapanese texlive-langkorean texlive-latexextra texlive-music texlive-pictures texlive-plainextra texlive-pstricks texlive-publishers texlive-science texlive-bibtexextra
 
 # Install python packages
 sudo -H pip install pip jedi json-rpc service_factory autoflake hy pycscope flake8
@@ -15,6 +14,15 @@ sudo npm install -g tern js-beautify eslint babel-eslint eslint-plugin-react vmd
 # Install lua dependencies
 sudo luarocks install luacheck
 sudo luarocks install lanes
+
+# Set current path
+SOURCE="${BASH_SOURCE[0]}"
+while [[ -h "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 # Get newest spacemacs
 spacemacsInstallationDir="${HOME}/.emacs.d"
@@ -29,15 +37,6 @@ cp .spacemacs ~/.spacemacs
 # Set base path
 installBaseDir="${HOME}/.spacemacsInstall"
 mkdir "${installBaseDir}"
-
-# Set current path
-SOURCE="${BASH_SOURCE[0]}"
-while [[ -h "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
-    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 # Add .profile as default value for .bash_profile
 if grep -Fxq "[[ -f ~/.profile ]] && . ~/.profile" ~/.bash_profile
@@ -65,10 +64,10 @@ end" >> "${fishConfigFile}"
 
 # Build gnu global
 globalBaseDir="${installBaseDir}/global"
-globalCurrentVersionExtractDir="${globalBaseDir}/global-6.5.5"
+globalCurrentVersionExtractDir="${globalBaseDir}/global-6.5.6"
 if [[ ! -d "${globalCurrentVersionExtractDir}" ]]; then
     mkdir "${globalBaseDir}"
-    wget -O "${globalBaseDir}/global.tar.gz" http://tamacom.com/global/global-6.5.5.tar.gz
+    wget -O "${globalBaseDir}/global.tar.gz" http://tamacom.com/global/global-6.5.6.tar.gz
     tar -xpvf  "${globalBaseDir}/global.tar.gz" --directory "${globalBaseDir}"
     cd "${globalCurrentVersionExtractDir}" || exit
     ./configure --with-exuberant-ctags=/usr/bin/ctags
@@ -95,15 +94,15 @@ fi
 cd "${DIR}" || exit
 
 # Install java interface server
-javaServerBaseDir="${installBaseDir}/eclim"
-javaServerCurrentVersionExtractDir="${javaServerBaseDir}/2.6.0"
-if [[ ! -d "${javaServerCurrentVersionExtractDir}" ]]; then
-    mkdir "${javaServerBaseDir}"
-    mkdir "${javaServerCurrentVersionExtractDir}"
-    wget -O "${javaServerCurrentVersionExtractDir}/eclim.jar" https://github.com/ervandew/eclim/releases/download/2.6.0/eclim_2.6.0.jar
-    java -Djava.net.useSystemProxies=true -jar "${javaServerCurrentVersionExtractDir}/eclim.jar"
-fi
-cd "${DIR}" || exit
+#javaServerBaseDir="${installBaseDir}/eclim"
+#javaServerCurrentVersionExtractDir="${javaServerBaseDir}/2.6.0"
+#if [[ ! -d "${javaServerCurrentVersionExtractDir}" ]]; then
+#    mkdir "${javaServerBaseDir}"
+#    mkdir "${javaServerCurrentVersionExtractDir}"
+#    wget -O "${javaServerCurrentVersionExtractDir}/eclim.jar" https://github.com/ervandew/eclim/releases/download/2.6.0/eclim_2.6.0.jar
+#    java -Djava.net.useSystemProxies=true -jar "${javaServerCurrentVersionExtractDir}/eclim.jar"
+#fi
+#cd "${DIR}" || exit
 
 # Prepare Go environment
 goPath="${HOME}/goWorkspace"
