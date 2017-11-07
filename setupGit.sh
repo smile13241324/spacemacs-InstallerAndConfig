80ddccde1ba7f89c85ce4c18db0bd6a2f61fb2e5
smile13241324 <smile13241324@gmail.com>
2017-11-07 20:13:29 +0100

    Remove invalid layers from windows dotspacemacs file

--- setupGit.sh
+++ setupGit.sh
@@ -3,5 +3,3 @@
 # Git config
 git config --global user.name "${USER}"
 git config --global user.email "${USER}@gmail.com"
-#git config --global credential.helper cache
-#git config --global push.default simple
